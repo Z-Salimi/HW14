824668d089b9b13e3777b888c7ce163412cca8a0
Z-Salimi <zahra.2002.salimi@gmail.com>
2024-10-07 23:31:02 +0330

    update allAlarms

--- src/components/AllAlarms.tsx
+++ src/components/AllAlarms.tsx
@@ -1,15 +1,39 @@
 import React from "react";
 import { AlarmList, IAlarmList } from "./AlarmList";
 
+export const AllAlarms: React.FC<IAlarmList> = ({ list, setList }) => {
+  const [sort, setSort] = React.useState<"time" | "title">("time");
 
+  const sortTitle = () => {
+    setSort("title");
+  };
 
-export const AllAlarms:React.FC<IAlarmList>= ({list}) => {
+  const sortTime = () => {
+    setSort("time");
+  };
+
   return (
-    <section className="w-[50vw] h-[35vh] px-2 py-5 flex flex-col gap-3 rounded-xl bg-purple-200">
+    <section className="w-[50vw] h-[35vh] px-2 py-5 flex flex-col items-center gap-3 rounded-xl bg-purple-200">
       <h2 className="text-2xl font-bold text-center mb-2 text-purple-950">
         Alarm Lists
       </h2>
-      <AlarmList list={list} />
+      <div className="flex items-center justify-around py-1 gap-5 w-[93%] rounded-lg bg-pink-800">
+        <button
+          type="button"
+          onClick={sortTitle}
+          className="rounded-md bg-purple-400 px-2 py-1 text-sm font-medium text-gray-900  hover:bg-purple-200 sm:w-auto"
+        >
+          Sort by Title
+        </button>
+        <button
+          type="button"
+          onClick={sortTime}
+          className="rounded-md bg-pink-200 px-2 py-1 text-sm font-medium text-gray-900  hover:bg-pink-100 sm:w-auto"
+        >
+          Sort by Time
+        </button>
+      </div>
+      <AlarmList list={list} setList={setList} sort ={sort}/>
     </section>
   );
 };
